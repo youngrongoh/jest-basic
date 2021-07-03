b36f1d96e61cee6cb9215addd7c567d43a11911f
Oh Young-rong <youngrong.oh@gmail.com>
2021-07-03 22:38:41 +0900

    테스트 작성 - test() - expect() - toBe() => matcher - not

--- src/fn.test.js
+++ src/fn.test.js
@@ -3,3 +3,12 @@ const fn = require("./fn.js");
 test("1은 1이야.", () => {
   expect(1).toBe(1);
 });
+
+test("2 더하기 3은 5야.", () => {
+  expect(fn.add(2, 3)).toBe(5);
+});
+
+// 실패 케이스
+test("3 더하기 3은 5가 아니야.", () => {
+  expect(fn.add(3, 3)).not.toBe(5);
+});
